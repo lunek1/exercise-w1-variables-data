f4337a093c41b2e8cfd2ffb41a464da4154efe8c
lunek1 <154337428+lunek1@users.noreply.github.com>
2024-02-06 14:54:19 +0100

    index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ Create a variable that stores the name of your cafe.
 Print out a greeting such as "Welcome to Technigo Cafe! What would you like to order today?"
 Where echnigo Cafe is replaced by the name of your cafe that is stored in the variable.
 */
-const cafeName = "Technigo Cafe";
+const cafeName = "Lightning Cafe";
 console.log(`Welcome to ${cafeName}! What would you like to order today?`);
 
 /*
@@ -15,12 +15,20 @@ Print out the total price such as "There you go, that'll be 10 euros"
 Where 10 is replaced by the calculation of the total price.
 */
 
+const coffeePrice = 3.5;
+const numberCoffee = 1;
+const totalPrice = coffeePrice * numberCoffee;
+console.log(`There you go, that'll be ${totalPrice} euros.`);
+
 /*
 3)
 Create a variable that stores a boolean.
 Print out "You said this coffee is the best, that was actually true"
 Where true is replaced by your varible
 */
+
+const isCoffeeBest = true;
+console.log(`You said this coffee is the best, that was actually ${isCoffeeBest}`);
 
 /*
 4)
@@ -30,6 +38,10 @@ Print it out.
 assign it a new value.
 Print it out. => This should give you the new value.
 */
+let cafeGuests = 3;
+console.log(cafeGuests);
+cafeGuests= 5;
+console.log(cafeGuests);
 
 /*
 5)
@@ -40,16 +52,25 @@ assign it a new value.
 Print it out. => This should give you an error because it shouldn't be able to be changed.
 */
 
+const maxGuests = 15;
+// maxGuests = 18; 
+console.log(maxGuests);
+
 /*
 6)
 Create a variable that stores a string.
 Print out that string in only UPPERCASE letters.
 */
 
+let text = `Lightning Cafe`;
+console.log(text.toUpperCase())
+
 /*
 7)
 Print out the same string in only lowercase letters.
 */
+let candy = "Candy wonderland";
+console.log(candy.toLowerCase());
 
 /*
 8) **BONUS**
@@ -58,3 +79,6 @@ Then figure out a way to replace the word "summer" in the string with the word "
 Should give you => "Today we have a special winter deal!"
 (check for a specific string method...)
 */
+const deal = "Today we have a special summer deal";
+console.log(deal.replace("summer", "winter"));
+
